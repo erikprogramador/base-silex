88ee846b8b78be178b8912db0a300d80b08dc35b
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-02-12 17:22:42 -0200

    Adição de sugestão de dependencias front-end

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,14 +2,10 @@
 
 namespace App\Controllers;
 
-use App\Models\User;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        $username = (new User('Your username'))->getUsername();
-        return $this->app['twig']
-                    ->render('index.twig', compact('username'));
+        return $this->app['twig']->render('index.twig');
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,5 @@ let mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js/')
    .sass('resources/sass/app.sass', 'public/css/')
+   .copy('node_modules/font-awesome/fonts/**', 'public/fonts/')
 
